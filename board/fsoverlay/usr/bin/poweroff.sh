5d4888b375bcfc78261f1598105855fbb56dfc85
Huynh Duc Dung <dunghd.it@gmail.com>
2026-08-30 00:19:29 +0800

    fix(a133): provide poweroff.sh and reboot.sh for ES menu

---
+++ board/fsoverlay/usr/bin/poweroff.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# ES Platform.cpp on several KNULLI boards calls poweroff.sh
+exec /usr/bin/knulli-shutdown --shutdown
---
+++ board/fsoverlay/usr/bin/reboot.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# ES Platform.cpp on several KNULLI boards calls reboot.sh
+exec /usr/bin/knulli-shutdown --reboot
